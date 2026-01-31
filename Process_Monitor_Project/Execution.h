55a1d00a3368c54874edcfbd649bdd57c066a400
Poly0n <adrian2005mag@hotmail.com>
2026-01-31 17:24:19 -0500

    Update Execution.h

--- Process_Monitor_Project/Execution.h
+++ Process_Monitor_Project/Execution.h
@@ -1,1 +1,17 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<vector>
+#include <windows.h>
+#include <TlHelp32.h>
+#include "Pdh.h"
 
+class Execution
+{
+private:
+public:
+	bool oneLinerCommand(const std::string& input) const;
+	bool isNumber(const std::string& number) const;
+	bool wordCommands(const std::string& input) const;
+	std::wstring stringToWString(const std::string& str);
+};
