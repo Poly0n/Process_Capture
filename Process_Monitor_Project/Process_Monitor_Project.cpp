ff407a3c29c3d3c182539cbf8ef5906fc795589f
Poly0n <adrian2005mag@hotmail.com>
2026-01-31 17:22:48 -0500

    Update Process_Monitor_Project.cpp

--- Process_Monitor_Project/Process_Monitor_Project.cpp
+++ Process_Monitor_Project/Process_Monitor_Project.cpp
@@ -1,9 +1,25 @@
 #include "Processes.h"
 
+void SetColor()
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	SetConsoleTextAttribute(
+		hConsole,
+		BACKGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE
+	);
+
+	return;
+}
+
 int main() {
+
+	SetColor();
+
 	Processes myProcesses;
 
 	myProcesses.execute();
+
 
 	return 0;
 }
