10517ba42d37c5a8bf1f3ffb3cda2bff87c3f157
Poly0n <adrian2005mag@hotmail.com>
2026-02-01 13:12:31 -0500

    Update Execution.h

--- Process_Monitor_Project/Execution.h
+++ Process_Monitor_Project/Execution.h
@@ -4,14 +4,32 @@
 #include<vector>
 #include <windows.h>
 #include <TlHelp32.h>
+#include <unordered_map>
 #include "Pdh.h"
+
+struct CpuSample {
+	ULONGLONG lastProcessTime = 0;
+	ULONGLONG lastSystemTime = 0;
+};
+
+struct ppIDInformation {
+	DWORD ppid = 0;
+	std::wstring ppName = {};
+};
 
 class Execution
 {
 private:
+	std::unordered_map<DWORD, CpuSample> cpuSamples;
+	ppIDInformation ppidInfo = {};
+	double fileTimeToSeconds(const FILETIME& ft);
+	ULONGLONG fileTimeToULL(const FILETIME& ft);
 public:
 	bool oneLinerCommand(const std::string& input) const;
 	bool isNumber(const std::string& number) const;
 	bool wordCommands(const std::string& input) const;
 	std::wstring stringToWString(const std::string& str);
+	void printProcessTimes(int processID, std::wstring& processName);
+	double GetProcessCpuUsage(HANDLE hProcess, CpuSample& sample);
+	void GetParentProcessID(DWORD pid, bool reset);
 };
